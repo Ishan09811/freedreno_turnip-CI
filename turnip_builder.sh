d161ce43e7e3e4e83876d0fb19a3949c1571ae5b
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2026-04-26 11:23:00 +0530

    Update turnip_builder.sh

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -102,7 +102,7 @@ prepare_workdir(){
 
 build_lib_for_android(){
 	echo "Creating meson cross file ..." $'\n'
-	ndk="$workdir/$ndkver/toolchains/llvm/prebuilt/linux-x86_64/bin"
+	ndk=$(find "$workdir" -maxdepth 1 -type d -name "android-ndk-*" | head -n 1)
 
 	cat <<EOF >"android-aarch64"
 [binaries]
